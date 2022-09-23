73a5ab202b139872081a48989093a7014545fd7e
Pedro <pedro_hasse@yahoo.com.br>
2022-09-23 17:09:49 -0300

    fix: 🐛 Bug do package-lock.json corrigido, maybe

---
+++ src/tests/helpers/Consts.js
@@ -0,0 +1,11 @@
+export const MIN_CHARACTERS = 7;
+
+export const EMAIL_INPUT_TESTID = 'email-input';
+export const PASSWORD_INPUT_TESTID = 'password-input';
+export const LOGIN_BUTTON_TESTID = 'login-submit-btn';
+
+export const VALID_EMAIL = 'goku@eusou.com';
+export const VALID_PASS = '123456';
+
+export const INVALID_EMAIL = 'souumemailinvalido';
+export const INVALID_PASS = '12345';
