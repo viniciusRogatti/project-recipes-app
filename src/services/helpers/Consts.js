27d259ced5ed5b4365f72fc442b74babbeeda044
vinicius <vinnyzsouza@gmail.com>
2022-09-24 07:51:06 -0300

    refatorando App.js

--- src/services/helpers/Consts.js
+++ src/services/helpers/Consts.js
@@ -1,13 +1,10 @@
-// LOGIN PAGE
-
-export const MIN_CHARACTERS = 7;
+// KEY'S LOCAL STORAGE
 export const USER_KEY = 'user';
 export const MEALS_KEY = 'mealsToken';
 export const DRINKS_KEY = 'drinksToken';
 
-export const LOGIN_PATH = '/';
-export const MEALS_PATH = '/meals';
-
+// LOGIN PAGE
+export const MIN_CHARACTERS = 7;
 export const EMAIL_INPUT_TESTID = 'email-input';
 export const PASSWORD_INPUT_TESTID = 'password-input';
 export const LOGIN_BUTTON_TESTID = 'login-submit-btn';
@@ -18,8 +15,17 @@ export const VALID_PASS = '1234567';
 export const INVALID_EMAIL = 'souumemailinvalido';
 export const INVALID_PASS = '12345';
 
-// SEARCH BAR
+// PATH NAMES
+export const LOGIN_PATH = '/';
+export const MEALS_PATH = '/meals';
+export const DRINKS_PATH = '/drinks';
+export const PROFILE_PATH = '/profile';
+export const DONE_RECIPES_PATH = '/done-recipes';
+export const FAVORITES_PATH = '/favorite-recipes';
+export const MEALS_RECIPES_PATH = '/meals/:id';
+export const DRINKS_RECIPES_PATH = '/drinks/:id';
 
+// SEARCH BAR COMPONENT
 export const INGREDIENT_SEARCH_TESTID = 'ingredient-search-radio';
 export const NAME_SEARCH_TESTID = 'name-search-radio';
 export const FIRST_LETTER_SEARCH_TESTID = 'first-letter-search-radio';
@@ -31,6 +37,5 @@ export const NAME_VALUE = 'name';
 export const FIRST_LETTER_VALUE = 'first-letter';
 
 // HEADER COMPONENT
-
 export const PROFILE_TOP_BTN = 'profile-top-btn';
 export const SEARCH_TOP_BTN = 'search-top-btn';
