7ae9e13a341dd88c7b08a8c75703c766f16b4a6c
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-09-26 20:29:10 -0300

    Tetando renderizar os ingredientes e as medidas

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -6,20 +6,55 @@ function RecipeDetails() {
   const { id } = useParams();
   const { pathname } = useLocation();
   const [details, setDetails] = useState([]);
-  console.log(pathname);
+  // console.log(pathname);
 
   useEffect(() => {
     const fetchDetails = async () => {
-      setDetails(await RecipeDetalsAPI(pathname, id));
+      const screen = pathname.includes('meals') ? 'meals' : 'drinks';
+      const response = await RecipeDetalsAPI(pathname, id);
+      const result = response[screen];
+      setDetails(result);
     };
     fetchDetails();
   }, [id, pathname]);
-  // console.log(id);
   console.log(details);
+  if (details[0] !== undefined) {
+    console.log(Object
+      .entries(details[0])
+      .filter((detaill) => detaill[0].includes('strIngredient' || 'strMeasure'))
+      .map((ingredient) => ingredient[1])
+      .filter((ingredient) => ingredient !== null));
+    console.log(Object
+      .entries(details[0])
+      .filter((detaill) => detaill[0].includes('strMeasure')));
+
+    console.log();
+  }
   return (
-    <div>
-      {id}
-    </div>
+    <section>
+      {
+        details.map((detail) => (
+          <div key={ detail.idDrink }>
+            <img alt={ detail.strDrink } src={ detail.strDrinkThumb } />
+            <h3>{detail.strDrink}</h3>
+            <h4>{detail.strCategory}</h4>
+            <p>{detail.strAlcoholic}</p>
+            <h5>
+              Ingredients
+            </h5>
+            {Object
+              .entries(details[0])
+              .filter((detaill) => detaill[0].includes('strIngredient'))
+              .map((ingredient) => ingredient[1])
+              .filter((ingredient) => ingredient !== null)
+              .map((ingredient, index) => (
+                <p key={ index }>{ingredient}</p>
+              ))}
+
+          </div>
+        ))
+      }
+    </section>
   );
 }
 
